ba73222e948254f531c7e7c35a284e2921c757c5
Reon Fernandes <reondias@gmail.com>
2026-01-04 16:18:35 +0530

    validation
    added basic validation for OrderRequest dto.

--- order-service/src/main/java/com/reon/orderservice/dto/OrderRequest.java
+++ order-service/src/main/java/com/reon/orderservice/dto/OrderRequest.java
@@ -1,5 +1,7 @@
 package com.reon.orderservice.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,7 +14,10 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @Builder
 public class OrderRequest {
+    @NotBlank(message = "Sku code is required.")
     private String skuCode;
+    @NotNull(message = "Order price cannot be null")
     private BigDecimal price;
+    @NotNull(message = "Quantity cannot be null")
     private Integer quantity;
 }
